7f010238f511b72dd9b98e9bc179c5cc2b35b17a
Kamel <kdjoudi@cloudera.com>
2021-08-19 16:39:52 +0200

    update bootstrap file

--- 0_bootstrap.py
+++ 0_bootstrap.py
@@ -32,21 +32,39 @@ except:
 run_time_suffix = datetime.datetime.now()
 run_time_suffix = run_time_suffix.strftime("%d%m%Y%H%M%S")
 
+# Set the setup variables needed by CMLBootstrap
+HOST = os.getenv("CDSW_API_URL").split(
+    ":")[0] + "://" + os.getenv("CDSW_DOMAIN")
+USERNAME = os.getenv("CDSW_PROJECT_URL").split(
+    "/")[6]
+API_KEY = os.getenv("CDSW_API_KEY")
+PROJECT_NAME = os.getenv("CDSW_PROJECT") 
+ 
 # Instantiate API Wrapper
-cml = CMLBootstrap()
-
+cml = CMLBootstrap(HOST, USERNAME, API_KEY, PROJECT_NAME)
+ 
 # Set the STORAGE environment variable
-try : 
+try :
   storage=os.environ["STORAGE"]
 except:
-  storage = cml.get_cloud_storage()
+  if os.path.exists("/etc/hadoop/conf/hive-site.xml"):
+    tree = ET.parse('/etc/hadoop/conf/hive-site.xml')
+    root = tree.getroot()
+    for prop in root.findall('property'):
+      if prop.find('name').text == "hive.metastore.warehouse.dir":
+        storage = prop.find('value').text.split("/")[0] + "//" + prop.find('value').text.split("/")[2]
+  else:
+    storage = "/user/" + os.getenv("HADOOP_USER_NAME")
   storage_environment_params = {"STORAGE":storage}
   storage_environment = cml.create_environment_variable(storage_environment_params)
   os.environ["STORAGE"] = storage
-
-# Upload the data to the cloud storage
-!hadoop fs -mkdir -p $STORAGE/datalake
-!hadoop fs -mkdir -p $STORAGE/datalake/data
-!hadoop fs -mkdir -p $STORAGE/datalake/data/churn
-!hadoop fs -copyFromLocal /home/cdsw/raw/WA_Fn-UseC_-Telco-Customer-Churn-.csv $STORAGE/datalake/data/churn/WA_Fn-UseC_-Telco-Customer-Churn-.csv
-
+ 
+#Verify STORAGE value
+!echo $STORAGE
+ 
+!hdfs dfs -ls $STORAGE
+!hdfs dfs -mkdir -p $STORAGE/datalake
+!hdfs dfs -mkdir -p $STORAGE/datalake/data
+!hdfs dfs -mkdir -p $STORAGE/datalake/data/churn
+!hdfs dfs -mkdir -p $STORAGE/telco/data/churn
+!hdfs dfs -put /home/cdsw/raw/WA_Fn-UseC_-Telco-Customer-Churn-limited.csv $STORAGE/datalake5/data/churn/WA_Fn-UseC_-Telco-Customer-Churn-.csv
